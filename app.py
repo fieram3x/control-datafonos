aed48d0f55d430666c609fa0ad7290a5663138b6
fieram3x <roldanymatos@gmail.com>
2026-05-12 14:11:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -376,12 +376,48 @@ def header():
 
 
 def login():
-    st.markdown("""
-    <div class="title-card">
-        <h1>Control de Datafonos</h1>
-        <p>Acceso seguro al panel profesional de control, resguardo, cambios y decomisos.</p>
+st.markdown("""
+<div class="title-card">
+    <div style="
+        display:flex;
+        align-items:center;
+        justify-content:center;
+        gap:16px;
+    ">
+
+        <div style="
+            font-size:48px;
+            background:#FFFFFF;
+            width:72px;
+            height:72px;
+            border-radius:20px;
+            display:flex;
+            align-items:center;
+            justify-content:center;
+            box-shadow:0 4px 15px rgba(0,0,0,0.08);
+        ">
+            💳
+        </div>
+
+        <div>
+            <h1 style="
+                margin:0;
+                text-align:left;
+            ">
+                Control de Datafonos
+            </h1>
+
+            <p style="
+                margin-top:8px;
+                text-align:left;
+            ">
+                Acceso al panel de control.
+            </p>
+        </div>
+
     </div>
-    """, unsafe_allow_html=True)
+</div>
+""", unsafe_allow_html=True)
 
     col1, col2, col3 = st.columns([1.2, 1, 1.2])
     with col2:
